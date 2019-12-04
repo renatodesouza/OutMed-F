690cae8491fb13a7f4daebcdc2bd9fd20de7aa90
gabelesss <gabriel.7rm@outlook.com>
2019-12-03 23:39:33 -0300

    Comentando linhas referentes ao SQL Alchemy. Sera utilizado mais tarde

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-database = SQLAlchemy(app)
+# database = SQLAlchemy(app)
 
 
 @app.route('/hello')
